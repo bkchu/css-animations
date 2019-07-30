1e32385c6f50d4252d0df65de72dae5bec4e0d98
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-29 21:30:26 -0500

    study the crap outta this

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    open: false
+  };
+
+  toggleMenu = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    let dropdownClassnames = 'dropdown';
+    if (this.state.open) {
+      dropdownClassnames += ' dropdown--open';
+    }
+
+    return (
+      <div className="App">
+        <nav className="navbar">
+          <div className="navbar__title">StartBootstrap</div>
+          <button className="navbar__menu" onClick={this.toggleMenu}>
+            Menu
+          </button>
+        </nav>
+        <div className={dropdownClassnames}>
+          <ul className="dropdown__list">
+            <li className="dropdown__item">Services</li>
+            <li className="dropdown__item">Portfolio</li>
+            <li className="dropdown__item">About</li>
+            <li className="dropdown__item">Team</li>
+            <li className="dropdown__item">Contact</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
